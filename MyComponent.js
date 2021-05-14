2bbfa3adc50bca5f37cfb92407f550876615a770
tmdckszm <78646428+tmdckszm@users.noreply.github.com>
2021-05-14 20:00:42 +0900

    Add files via upload

--- MyComponent.js
+++ MyComponent.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-const MyComponent = () => {
-    return <div>나의 새롭고 멋진 컴포넌트</div>;
+const MyComponent = ({name, children}) => {
+    return(
+        <div>
+            안녕하세요, 제 이름은 {name}입니다. <br/>
+            children 값은 {children}
+            입니다.
+        </div>
+    );
+};
+
+MyComponent.defaultProps = {
+    name: '기본이름'
+};
+
+MyComponent.prototype = {
+    name: propTypes.string
 };
 
 export default MyComponent;
