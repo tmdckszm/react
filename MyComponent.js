917fef8d4a8f42263540c04e01d2e0fa02d2f4bd
tmdckszm <78646428+tmdckszm@users.noreply.github.com>
2021-05-15 21:24:10 +0900

    Add files via upload

--- MyComponent.js
+++ MyComponent.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import propTypes from 'prop-types';
 
-const MyComponent = ({name, children}) => {
-    return(
-        <div>
-            안녕하세요, 제 이름은 {name}입니다. <br/>
-            children 값은 {children}
-            입니다.
-        </div>
-    );
-};
+class MyComponent extends Component{
+    render(){
+        const {name, favoriteNumber, children} = this.props;
+        return(
+            <div>
+                안녕하세요, 제 이름은 {name}입니다. <br/>
+                children 값은 {children}
+                입니다.
+                <br/>
+                제가 좋아하는 숫자는 {favoriteNumber}입니다.
+            </div>
+        );
+    }
 
+};
 MyComponent.defaultProps = {
     name: '기본이름'
 };
 
 MyComponent.prototype = {
-    name: propTypes.string
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
 };
 
 export default MyComponent;
