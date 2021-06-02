f1030bea1f40ab24f4d453aae3b86d018047700b
tmdckszm <78646428+tmdckszm@users.noreply.github.com>
2021-06-02 23:11:54 +0900

    Add files via upload

---
+++ Toc.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Toc extends Component{
+    render (){
+        var lists =[];
+        var data = this.props.data;
+        var i =0;
+        while(i<data.length){
+            lists.push(
+            <li key={data[i].id}>
+                <a 
+                  href={"/content/"+data[i].id}
+                  data-id={data[i].id}
+                  onClick={(e)=>{  
+                    e.preventDefault();
+                    this.props.onChangePage(e.target.dataset.id);
+                  }}
+                  >{data[i].title}</a>
+            </li>);
+
+            i+=1;
+        }
+        return(
+            <nav>
+                <ul>
+                    {lists}
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Toc;
