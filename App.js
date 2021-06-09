ea858911b2ffa7684048a4229253f6a11b2fe299
tmdckszm <78646428+tmdckszm@users.noreply.github.com>
2021-06-09 23:41:47 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,77 +1,50 @@
 import React, { Component } from 'react';
-import Toc from "./Toc"
-import Content from "./Content"
-import Subject from './Subject';
 
-class App extends Component {
+class App extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            mode: 'read',
-            selected_content_id:2,
-            welcome:{title: 'welcome', desc:'Hello, React'},
-            subject:{title:'WEB', sub:'world wide web!'},
-            contents:[
-                {id:1, title:'HTML', desc:'HTML is HyperText for impormation'},
-                {id:2, title:'CSS', desc:'CSS is for design'},
-                {id:3, title:'Javascript', desc:'JavaScript is for interactive'},
-            ]
+        this.state ={
+            first: Math.ceil(Math.random()*9),
+            second: Math.ceil(Math.random()*9),
+            value: '',
+            result: '',
+        };
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        if(parseInt(this.state.value) === this.state.first * this.state.second){
+            this.setState({
+                first: Math.ceil(Math.random()*9),
+                second: Math.ceil(Math.random()*9),
+                value: '',
+                result: '정답:'+ this.state.value,
+            });
+        }
+        else{
+            this.setState({
+                value: '',
+                result: '땡',
+            });
         }
     }
-    render () {
-        var _title = null;
-        var _desc = null;
-        if(this.state.mode === 'welcome'){
-            _title = this.state.welcome.title;
-            _desc = this.state.welcome.desc;
-        }else if(this.state.mode ==='read'){
-            var i =0;
-            while(i<this.state.contents.length){
-                var data = this.state.contents[i];
-                if(data.id === this.state.selected_content_id){
-                    _title = data.title;
-                    _desc = data.desc;
-                    break;
-                }
-                i+=1;
-            }
-        }
+
+    onChange = (e) => {
+        this.setState({
+            value: e.target.value,
+        });
+    };
+
+    render(){
         return (
-            <div className ="App">
-                <Subject
-                    title={this.state.subject.title}
-                    sub={this.state.subject.sub}
-                    onChangePage={()=>{
-                        this.setState({
-                            mode: 'welcome'
-                        });
-                    }}
-                >
-                </Subject>
-               {/* 
-                <header>
-                    <h1><a href="/" onClick={(e)=>{
-                        console.log(e);
-                        e.preventDefault();
-                        this.setState({
-                            mode:'welcome'
-                        });
-                    }}
-                    >{this.state.subject.title}</a></h1>
-                    {this.state.subject.sub}
-                </header>
-               */} 
-                <Toc onChangePage={(id)=>{
-                    this.setState({
-                        mode:'read',
-                        selected_content_id: Number(id)
-                    });
-                }}
-                data={ this.state.contents }
-                ></Toc>
-                <Content title={_title} desc = {_desc}
-                ></Content>
-            </div>
+          <div>
+              <h2>{this.state.first}곱하기{this.state.second}은 무엇일까요?</h2>
+              <form onSubmit ={this.onSubmit}>
+                  <input type="number" value={this.state.value} onChange={this.onChange}/>
+                  <button>입력</button>
+              </form>
+              <h2>{this.state.result}</h2>
+          </div>  
         );
     }
 }
