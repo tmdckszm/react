9d27624af6125f203c975df6ffea9863c3ade9b6
tmdckszm <78646428+tmdckszm@users.noreply.github.com>
2021-06-10 18:14:52 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,52 +1,39 @@
-import React, { Component } from 'react';
+import React, {useRef, useState } from 'react';
 
-class App extends Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            first: Math.ceil(Math.random()*9),
-            second: Math.ceil(Math.random()*9),
-            value: '',
-            result: '',
-        };
-    }
+const App = () =>{
+    const[word, setWord] = useState('승찬');
+    const[result, setResult] = useState('');
+    const[value, setValue] = useState('');
+    const inputRef = useRef(null);
 
-    onSubmit = (e) => {
+    const onSubmitInput = (e) => {
         e.preventDefault();
-        if(parseInt(this.state.value) === this.state.first * this.state.second){
-            this.setState({
-                first: Math.ceil(Math.random()*9),
-                second: Math.ceil(Math.random()*9),
-                value: '',
-                result: '정답:'+ this.state.value,
-            });
+        if(word[word.length-1] === value[0]){
+            setWord(value);
+            setResult('딩동댕');
+            setValue('');
         }
         else{
-            this.setState({
-                value: '',
-                result: '땡',
-            });
+            setResult('땡');
+            setValue('');
         }
-    }
-
-    onChange = (e) => {
-        this.setState({
-            value: e.target.value,
-        });
     };
 
-    render(){
-        return (
-          <div>
-              <h2>{this.state.first}곱하기{this.state.second}은 무엇일까요?</h2>
-              <form onSubmit ={this.onSubmit}>
-                  <input type="number" value={this.state.value} onChange={this.onChange}/>
-                  <button>입력</button>
-              </form>
-              <h2>{this.state.result}</h2>
-          </div>  
-        );
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
     }
+
+
+    return(
+        <div>
+            <h2>{word}</h2>
+            <form onSubmit={onSubmitInput}>
+                <input ref={inputRef} value={value} onChange={onChangeInput}/>
+                <button>입력</button>
+            </form>
+            <h2>{result}</h2>
+        </div>
+    );
 }
 
 export default App;
