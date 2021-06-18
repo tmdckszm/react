2b7103fb253166e2c6143e9e1df412eccc5766d6
tmdckszm <78646428+tmdckszm@users.noreply.github.com>
2021-06-18 20:50:57 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,39 +1,14 @@
-import React, {useRef, useState } from 'react';
+import React, { Component, Fragment } from 'react';
+import BaseBallGame from './BaseBallGame';
 
-const App = () =>{
-    const[word, setWord] = useState('승찬');
-    const[result, setResult] = useState('');
-    const[value, setValue] = useState('');
-    const inputRef = useRef(null);
 
-    const onSubmitInput = (e) => {
-        e.preventDefault();
-        if(word[word.length-1] === value[0]){
-            setWord(value);
-            setResult('딩동댕');
-            setValue('');
-        }
-        else{
-            setResult('땡');
-            setValue('');
-        }
-    };
-
-    const onChangeInput = (e) => {
-        setValue(e.target.value);
+class App extends Component{
+    render(){
+        return(
+            <Fragment>
+                <BaseBallGame/>
+            </Fragment>
+        );
     }
-
-
-    return(
-        <div>
-            <h2>{word}</h2>
-            <form onSubmit={onSubmitInput}>
-                <input ref={inputRef} value={value} onChange={onChangeInput}/>
-                <button>입력</button>
-            </form>
-            <h2>{result}</h2>
-        </div>
-    );
 }
-
 export default App;
