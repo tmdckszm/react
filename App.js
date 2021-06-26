d568e68d67b2e338c546ee4acdc2cebfba83326e
tmdckszm <78646428+tmdckszm@users.noreply.github.com>
2021-06-26 23:01:05 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,57 @@
-import React, { Component, Fragment } from 'react';
-import BaseBallGame from './BaseBallGame';
+import React, { Component, Fragment, useState} from 'react';
+import Movie from './Movie'
+import './App.css'
 
+const App = () => {
+    const [movieTitle, setMovieTitle]= useState('');
+    const [movieYear, setMovieYear]= useState('');
+    const [movies, setMovies]= useState([
+        { title: 'kossie coder1', year: 2001},
+        { title: 'kossie coder2', year: 2002},
+        { title: 'kossie coder3', year: 2003},
+        { title: 'kossie coder4', year: 2004},
+    ]);
 
-class App extends Component{
-    render(){
-        return(
-            <Fragment>
-                <BaseBallGame/>
-            </Fragment>
+    const renderMovies = movies.map((movie)=>{
+        return (
+            <Movie movie={movie} key={movie.title}/>
         );
+    })
+
+    const addMovie = (e) => {
+        e.preventDefault();
+        setMovies([
+            ...movies,
+            {
+                title: movieTitle,
+                year: movieYear
+            }
+        ])
+        setMovieTitle('');
+        setMovieYear('');
+
     }
+    return(
+        <div className="App">
+            <h1>Movie list</h1>
+            <form onSubmit={addMovie}>
+                <input
+                 type='text'
+                 placeholder='영화제목'
+                 value={movieTitle}
+                 onChange={(e) => setMovieTitle(e.target.value)}
+                /><br/>
+                <input
+                 type='text'
+                 placeholder='개봉년도'
+                 value={movieYear}
+                 onChange={(e) => setMovieYear(e.target.value)}
+                /><br/>
+                <button type='onSubmot'>영화추가</button>
+            </form>
+            {renderMovies}
+        </div>
+    );
 }
+
 export default App;
